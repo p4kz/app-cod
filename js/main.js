fb7654a9f4da41899d874aa4c7647b354c0532a0
p4kz <wolpak@hotmail.com>
2022-04-17 10:35:05 -0300

    add encrypt and decrypt

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const textUser = document.querySelector('#textUser')
+const result = document.querySelector('.result')
+const copy = document.querySelector('.btn-copy')
+const msgCopy = document.querySelector('.msg-copy')
+
+let textTemp = ''
+
+function encrypt() {
+  if (textUser.value != "") {
+    textTemp = textUser.value.replace(/e/g, "enter");
+    textTemp = textTemp.replace(/i/g, "imes");
+    textTemp = textTemp.replace(/a/g, "ai");
+    textTemp = textTemp.replace(/o/g, "ober");
+    textTemp = textTemp.replace(/u/g, "ufat");
+
+    result.textContent =textTemp;
+    copy.style.display = 'initial'
+    textUser.value = "";
+  }
+}
+
+function decrypt() {
+  if (textUser.value != "") {
+    textTemp = textUser.value.replace(/enter/g, "e");
+    textTemp = textTemp.replace(/imes/g, "i");
+    textTemp = textTemp.replace(/ai/g, "a");
+    textTemp = textTemp.replace(/ober/g, "o");
+    textTemp = textTemp.replace(/ufat/g, "u");
+
+    result.textContent = textTemp;
+    copy.style.display = 'initial'
+    textUser.value = "";
+  }
+}
+
+function copyText() {
+  navigator.clipboard.writeText(result.textContent);
+  setTimeout(msgCopy.style.display = 'initial', 1000)
+}
